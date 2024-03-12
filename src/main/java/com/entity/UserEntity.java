3962cc3cf045f571aeb80d29f84e42722e8a36f9
Tejas Shah <tejasshah2k19@gmail.com>
2024-03-12 19:32:53 +0530

    first

---
+++ src/main/java/com/entity/UserEntity.java
@@ -0,0 +1,61 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity //class-table ->create table 
+@Table(name="users")
+public class UserEntity {
+
+	@Id //primary key 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//1 2 3 4 
+	private Integer userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
